d4f1daff7a18f093454705e09e6eb81a648f9176
rumanparvz <62352795+rumanparvz@users.noreply.github.com>
2020-11-18 22:42:53 +0600

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,28 @@
+var preloader = document.getElementById('loader');
+function preLoaderHandler(){
+    preloader.style.display = 'none';
+}
+
+
+ 	  var loader;
+
+       function loadNow(opacity) {
+           if (opacity <= 0) {
+               displayContent();
+           } else {
+               loader.style.opacity = opacity;
+               window.setTimeout(function() {
+                   loadNow(opacity - 0.05);
+               }, 200);
+           }
+       }
+       
+       function displayContent() {
+           loader.style.display = 'none';
+           document.getElementById('content').style.display = 'block';
+       }
+       
+       document.addEventListener("DOMContentLoaded", function() {
+           loader = document.getElementById('loader');
+           loadNow(1);
+       });	
